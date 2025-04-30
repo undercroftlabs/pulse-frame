59f52e20d1b532cc92f1cbe044046b2b48b3a944
Michael Goodwin <mikejg101@icloud.com>
2025-04-30 19:21:02 -0400

    chore: remove unnecessary globals from rollup and correct package name

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,6 @@
 const { withUndercroftRollupConfig } = require('@undercroft/lib-tools');
-const nodePolyfills = require('rollup-plugin-polyfill-node');
 
 module.exports = withUndercroftRollupConfig({
-  name: 'Pulse',
-  bundle: 'undercroftjs/pulse',
-  globals: {
-    http: 'http',
-    crypto: 'crypto',
-    zlib: 'zlib',
-  },
-  plugins: [nodePolyfills()],
+  name: 'PulseFrame',
+  bundle: 'undercroftjs/pulse-frame',
 });
